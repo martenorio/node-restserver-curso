e8097c4f1e902b7022e2ce4bfbf59053004fffc3
chuchunmaru <martcecyte@gmail.com>
2020-04-04 16:27:25 -0600

    Google Sing-In

--- server/config/config.js
+++ server/config/config.js
@@ -42,3 +42,8 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+//==========================================================
+//  Google client ID
+//==========================================================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '932508311189-vl5v4efus757hh2ja8cgmfvi0f7rlfi7.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -3,13 +3,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser')
-
+const path = require('path');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 //Configuration global to Routes
+
+//habilitar la carpeta publica
+app.use(express.static(path.resolve(__dirname , '../public')));
+
 // importa todas la rutas que se ocuparan
 app.use(require('./routes/index'))
 
